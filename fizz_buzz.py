d85bfd1d4e76bf5de826085f29071b1eb3211245
Niharika Vishwakarma <niharika.vishwakarma08@gmail.com>
2022-05-15 15:42:20 +0530

    Update fizz_buzz.py

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -2,14 +2,15 @@
 #multiple of  will print -buzz
 #multiple of 3&5 will print -fizz-buzz 
 
+def fizzbuzz(r):
+    for i in range(1,r):
+         if (i%3==0 and i%5==0):
+           print(i,"fizz-buzz")
+         elif (i%5==0):
+           print(i,"buzz")
+         elif (i%3==0):
+           print(i,"fizz")
+         else:
+            print(i,"empty")
 
-for i in range(1,51):
-    if (i%3==0 and i%5==0):
-       print(i,"fizz-buzz")
-    elif (i%5==0):
-        print(i,"buzz")
-    elif (i%3==0):
-        print(i,"fizz")
-    else:
-        print(i,"empty")
-  
+fizzbuzz(34)
